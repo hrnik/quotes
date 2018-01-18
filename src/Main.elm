79eca4dd74ec0762fabf5ba92172436c83095b79
Nikita Khristinin <nkhristinin@gmail.com>
2018-01-18 23:05:13 +0200

    Some draft of quotes

--- src/Main.elm
+++ src/Main.elm
@@ -2,43 +2,183 @@ module Main exposing (..)
 
 import Html exposing (Html, text, div, h1, img)
 import Html.Attributes exposing (src)
+import WebSocket
+import Dict exposing (Dict)
+import Json.Decode exposing (..)
 
 
 ---- MODEL ----
 
 
+type Grow
+    = UP
+    | DOWN
+    | STAY
+
+
+isGrow old new =
+    if old == new then
+        STAY
+    else if old > new then
+        DOWN
+    else
+        UP
+
+
+type alias InstrumentFromSocket =
+    { s : String, b : String, a : String, spr : String }
+
+
+type alias Instrument =
+    { name : String
+    , bid : Float
+    , bidGrow : Grow
+    , ask : Float
+    , askGrow : Grow
+    }
+
+
+subscribeInsrtrumentNames =
+    [ "EURUSD", "BTCUSD" ]
+
+
+subscribeString instruments =
+    "SUBSCRIBE: " ++ (String.join "," instruments)
+
+
 type alias Model =
-    {}
+    { instruments : Dict String Instrument
+    , message : String
+    }
+
+
+quotesServer : String
+quotesServer =
+    "wss://quotes.exness.com:18400/"
+
+
+initialCmd =
+    subscribeString subscribeInsrtrumentNames
+        |> WebSocket.send quotesServer
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { instruments = Dict.empty
+      , message = ""
+      }
+    , initialCmd
+    )
 
 
 
 ---- UPDATE ----
 
 
+instrumentDecoder =
+    map4 InstrumentFromSocket (field "s" string) (field "b" string) (field "a" string) (field "spr" string)
+
+
+responseDecoder =
+    field "ticks" (list instrumentDecoder)
+
+
 type Msg
-    = NoOp
+    = Echo String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Echo response ->
+            let
+                newInstruments =
+                    case decodeString responseDecoder response of
+                        Err msg ->
+                            model.instruments
+
+                        Ok instruments ->
+                            instruments
+                                |> List.map
+                                    (\instrument ->
+                                        case Dict.get instrument.s model.instruments of
+                                            Nothing ->
+                                                ( instrument.s, (Instrument instrument.s 0 STAY 0 STAY) )
+
+                                            Just oldInstrument ->
+                                                let
+                                                    newBid =
+                                                        Result.withDefault 0 (String.toFloat instrument.b)
+
+                                                    newAsk =
+                                                        Result.withDefault 0 (String.toFloat instrument.a)
+
+                                                    bidGrow =
+                                                        isGrow oldInstrument.bid newBid
+
+                                                    askGrow =
+                                                        isGrow oldInstrument.ask newAsk
+                                                in
+                                                    ( instrument.s, (Instrument instrument.s newBid bidGrow newAsk askGrow) )
+                                    )
+                                |> Dict.fromList
+            in
+                ( { model | instruments = Dict.union newInstruments model.instruments }, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
+growText grow =
+    case grow of
+        STAY ->
+            " stay"
+
+        UP ->
+            " up"
+
+        DOWN ->
+            " down"
+
+
+viewInstrument : Instrument -> Html Msg
+viewInstrument instrument =
+    div []
+        [ div [] [ text instrument.name ]
+        , div []
+            [ text "BID: "
+            , text <| toString <| instrument.bid
+            , text (growText instrument.bidGrow)
+            ]
+        , div []
+            [ text "ASK: "
+            , text <| toString <| instrument.ask
+            , text (growText instrument.askGrow)
+            ]
+        ]
+
+
 view : Model -> Html Msg
 view model =
     div []
         [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        , h1 []
+            (model.instruments
+                |> Dict.toList
+                |> List.map Tuple.second
+                |> List.map viewInstrument
+            )
         ]
+
+
+
+---- SUBSCRIPTIONS ----
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    WebSocket.listen quotesServer Echo
 
 
 
@@ -51,5 +191,5 @@ main =
         { view = view
         , init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
